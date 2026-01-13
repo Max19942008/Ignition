63a59f2f4527bb2bba06c8e687c9bd139e4fbe67
Rakhimboev Bobur <boburrahimboyev2008@gmail.com>
2026-01-13 12:20:32 +0900

    fix: modify the like toggle logic in Detail page

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -11,7 +11,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import WestIcon from '@mui/icons-material/West';
 import EastIcon from '@mui/icons-material/East';
-import { useQuery, useReactiveVar } from '@apollo/client';
+import { useMutation, useQuery, useReactiveVar } from '@apollo/client';
 import { useRouter } from 'next/router';
 import { Property } from '../../libs/types/property/property';
 import moment from 'moment';
@@ -29,7 +29,9 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { GET_PROPERTIES, GET_PROPERTY } from '../../apollo/user/query';
 import { T } from '../../libs/types/common';
-import { Direction } from '../../libs/enums/common.enum';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -57,6 +59,8 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 	});
 
 	/** APOLLO REQUESTS **/
+	const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY)
+
 		const {
 		loading: getPropertyLoading, 
 		data: getPropertyData, 
@@ -126,6 +130,37 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 		commentInquiry.page = value;
 		setCommentInquiry({ ...commentInquiry });
 	};
+
+	const likePropertyHandler = async (user: T, id:string) => {
+			try {
+				if(!id) return;
+				if(!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+
+			await likeTargetProperty({
+				variables: { input: id }, 
+			});
+			 await getPropertyRefetch({ input: id });
+
+			 await getPropertiesRefetch({input: {
+				 page: 1,
+				limit: 4,
+				sort: "createdAt",
+				direction: Direction.DESC,
+				search: {
+					locationList: [property?.propertyLocation],
+				},
+			 },
+			});
+	
+			 await sweetTopSmallSuccessAlert("success", 800);
+	
+			} catch(err: any) {
+			console.log("ERROR likePropertyHandler:", err.message);
+			sweetMixinErrorAlert(err.message).then();
+			}
+		};
+
+   /** RENDER **/
 
 	if (device === 'mobile') {
 		return <div>PROPERTY DETAIL PAGE</div>;
@@ -202,15 +237,19 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 											<RemoveRedEyeIcon fontSize="medium" />
 											<Typography>{property?.propertyViews}</Typography>
 										</Stack>
-										<Stack className="button-box">
+										<Stack 
+											className="button-box" 
+											sx={{ cursor: 'pointer' }}
+											onClick={() => {
+												if (user && property?._id) {
+													likePropertyHandler(user, property._id);
+												}
+											}}
+										>
 											{property?.meLiked && property?.meLiked[0]?.myFavorite ? (
 												<FavoriteIcon color="primary" fontSize={'medium'} />
 											) : (
-												<FavoriteBorderIcon
-													fontSize={'medium'}
-													// @ts-ignore
-													onClick={() => likePropertyHandler(user, property?._id)}
-												/>
+												<FavoriteBorderIcon fontSize={'medium'} />
 											)}
 											<Typography>{property?.propertyLikes}</Typography>
 										</Stack>
@@ -560,7 +599,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 										{destinationProperty.map((property: Property) => {
 											return (
 												<SwiperSlide className={'similar-homes-slide'} key={property.propertyTitle}>
-													<PropertyBigCard property={property} key={property?._id} />
+													<PropertyBigCard property={property} key={property?._id} likePropertyHandler={likePropertyHandler} />
 												</SwiperSlide>
 											);
 										})}
